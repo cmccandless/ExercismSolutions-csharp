cc28cdb52b7df797fa0c4c38f2e83df206e3c9d7
cmccandless <crm1994@gmail.com>
2017-02-11 17:11:46 -0600

    New revision for C#/Diamond.cs

--- diamond/Diamond.cs
+++ diamond/Diamond.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 public class Diamond
 {
-	public static string Make(char ch)
-	{
-		var result = new List<string>();
-		for (char x='A';x<=ch;x++)
-		{
-			for (int i = 0; i < result.Count; i++) result[i] = ' ' + result[i] + ' ';
-			if (x == 'A') result.Add("A");
-			else result.Add(x + string.Join(string.Empty,Enumerable.Repeat(" ",2*result.Count-1)) + x);
-		}
-		return string.Join("\n",result.Concat(result.ToArray().Reverse().Skip(1)));
-	}
+    private static string Fill(char s, int left, int right) =>
+        string.Join("", new[] { "".PadLeft(left), s.ToString(), "".PadRight(right) });
+
+    private static IEnumerable<T> Mirror<T>(IEnumerable<T> col) =>
+        col.Concat(col.Reverse().Skip(1));
+
+    private static string Line(char c, char max) =>
+        string.Join("", Mirror(Fill(c, max - c, c - 'A')));
+
+    private static IEnumerable<char> CharRange(char stop, char start = 'A') =>
+        new int[stop - start + 1].Select((_, c) => (char)(c + 'A'));
+
+    public static string Make(char ch) =>
+        string.Join("\n", Mirror(CharRange(ch).Select(c => Line(c, ch))));
 }
