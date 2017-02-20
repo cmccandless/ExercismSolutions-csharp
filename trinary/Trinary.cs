7dc5030df75a147c75b0576a454abe09024221f0
cmccandless <crm1994@gmail.com>
2017-02-19 21:21:40 -0600

    Remove deprecated exercises

--- trinary/Trinary.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Linq;
-
-static class Trinary
-{
-    private static bool Invalid(char ch) => ch > '2' || ch < '0';
-
-    public static int ToDecimal(string trinary) =>
-        trinary.Any(Invalid) ? 0 : trinary.Aggregate(0, (a, x) => a * 3 + x - '0');
-}
