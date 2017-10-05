1cbc07dec364e4b8192c40e653772b33ef8bba72
Corey McCandless <crm1994@gmail.com>
2017-10-05 12:01:21 -0500

    new revision for csharp/collatz-conjecture

--- collatz-conjecture/CollatzConjecture.cs
+++ collatz-conjecture/CollatzConjecture.cs
@@ -5,6 +5,9 @@ public static class CollatzConjecture
     public static int Steps(int number) 
     {
         if (number < 1) throw new ArgumentException();
-        return number == 1 ? 0 : 1 + Steps(number % 2 == 0 ? number / 2 : number * 3 + 1);
+        int count;
+        for (count = 0; number != 1; count++)
+            number = number % 2 == 0 ? number / 2 : number * 3 + 1;
+        return count;
     }
 }
