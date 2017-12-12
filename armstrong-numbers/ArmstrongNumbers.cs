0d3a719a43959f053d90bd40e0d562f8f9bc7d12
Corey McCandless <crm1994@gmail.com>
2017-12-12 15:31:07 -0600

    new solutions to python, csharp exercises

---
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        IEnumerable<int> powNdigits(Char[] digits) => digits.Select(d => (int)Math.Pow(d - '0',  digits.Length));
+        return powNdigits(number.ToString().ToCharArray()).Sum() == number;
+    }
+}
