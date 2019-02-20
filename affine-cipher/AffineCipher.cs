eab0f74924dd40e8e850d21504051d4f0cf73fef
Corey McCandless <corey.mccandless@atlasied.com>
2019-02-20 11:07:33 -0500

    affine-cipher: new revision

--- affine-cipher/AffineCipher.cs
+++ affine-cipher/AffineCipher.cs
@@ -2,51 +2,86 @@
 using System.Collections.Generic;
 using System.Linq;
 
+static class Extensions
+{
+    public static IEnumerable<List<T>> Slices<T>(this IEnumerable<T> col, int sliceSize)
+    {
+        var q = new Queue<T>(col);
+        while(q.Any())
+        {
+            var l = new List<T>();
+            for (int i=0; i < sliceSize && q.Any(); i++)
+                l.Add(q.Dequeue());
+            yield return l;
+        }
+    }
+
+    public static IEnumerable<string> Slices(this string str, int sliceSize) =>
+        str.ToCharArray().Slices(sliceSize).Select(l => new String(l.ToArray()));
+
+    public static int LetterToIndex(this char ch, char _base = 'a') => ch - _base;
+    public static char IndexToLetter(this int index, char _base = 'a', int alphaSize = 26) =>
+        (char)((index % alphaSize) + _base);
+
+    public static char Transcode(this char ch, Func<int, int> transcoder) =>
+        transcoder(ch.LetterToIndex()).IndexToLetter();
+
+    public static string Transcode(this string str, Func<int, int> transcoder) =>
+        str.ToLower()
+            .Where(Char.IsLetterOrDigit)
+            .Select(ch =>
+                Char.IsLetter(ch) ? ch.Transcode(transcoder) : ch
+            ).ToArray()
+            .Join()
+            .Trim();
+
+    public static string Join<T>(this IEnumerable<T> col, string delimiter = "") =>
+        col.Join(delimiter, x => x.ToString());
+
+    public static string Join<T>(this IEnumerable<T> col, string delimiter, Func<T, string> formatter) =>
+        string.Join(delimiter, col.Select(formatter));
+}
+
 public static class AffineCipher
 {
     private static char[] letters = Enumerable.Range(0, 26).Select(i => (char)(i + 'a')).ToArray();
-    private static int M => letters.Length;
+    private static int ALPHA_SIZE => letters.Length;
+
     private static (int x, int y) minMax(int x, int y) => (Math.Min(x, y), Math.Max(x, y));
+
     private static int GCD((int x, int y) t) =>
         t.x == 0 ? t.y : GCD(minMax(t.x, t.y % t.x));
+
     private static int GCD(int x, int y) => GCD(minMax(x, y));
+
     private static bool Coprime(int x, int y) => GCD(x, y) == 1;
-    private static string Transcode(string text, Func<char, int, string> transcoder) =>
-        string.Join("", text.ToLower().Where(Char.IsLetterOrDigit).Select(transcoder)).Trim();
+
     public static string Encode(string plainText, int a, int b)
     {
-        if (!Coprime(a, M)) throw new ArgumentException();
-        string encode(char ch, int i) =>
-            (
-                Char.IsLetter(ch) ?
-                (char)(((a * (ch - 'a') + b) % M) + 'a') :
-                ch
-            ).ToString() +
-            ((i != 0 && i % 5 == 4) ? " " : "");
-        return Transcode(plainText, encode);
-    }
-    private static T Dump<T>(this T obj, string msg="")
-    {
-        Console.Out.WriteLine(msg + obj.ToString());
-        return obj;
+        if (!Coprime(a, ALPHA_SIZE)) throw new ArgumentException();
+        return plainText.Transcode(x => a * x + b).Slices(5).Join(" ");
     }
 
     public static string Decode(string cipheredText, int a, int b)
     {
-        if (!Coprime(a, M)) throw new ArgumentException();
+        if (!Coprime(a, ALPHA_SIZE)) throw new ArgumentException();
         int mmi;
-        for (mmi = 1; (a * mmi) % M != 1 && mmi < M; mmi++);
-        Func<char, int, string> decode(int _mmi) =>
-            (char ch, int i) =>
-                (
-                    Char.IsLetter(ch)
-                    ? (
-                        (char)(
-                            ((_mmi * (ch - 'a' - b + 2 * M)) % M) + 'a'
-                        )
-                    )
-                    : ch
-                ).ToString();
-        return Transcode(cipheredText, decode(mmi));
+        for (mmi = 1; (a * mmi) % ALPHA_SIZE != 1 && mmi < ALPHA_SIZE; mmi++);
+        Func<int, int> decode(int _mmi) =>
+            (int index) => _mmi * (index - b + 2 * ALPHA_SIZE);
+
+        return cipheredText.Transcode(decode(mmi));
     }
+}
+
+static class DebugExtensions
+{
+    public static T Dump<T>(this T obj, Func<T, string> formatter, string message = null)
+    {
+        var s = message == null ? formatter(obj) : $"{message}: {formatter(obj)}";
+        Console.WriteLine(s);
+        return obj;
+    }
+
+    public static T Dump<T>(this T obj, string message = null) => obj.Dump(x => x.ToString(), message);
 }
